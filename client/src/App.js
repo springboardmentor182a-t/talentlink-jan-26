41426a06922bc47470673b1142dbefdf90a5c05d
Akhila vk <akhilavk114@gmail.com>
2026-02-09 18:23:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+
 import ChooseRole from "./pages/auth/ChooseRole";
 import ClientLogin from "./pages/auth/ClientLogin";
 import ClientSignup from "./pages/auth/ClientSignup";
