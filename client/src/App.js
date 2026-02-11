1ce9c1ffd341236e1d7cd2e01a0be1a6eb76aa23
Akhila vk <akhilavk114@gmail.com>
2026-02-11 20:23:08 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+
 import ChooseRole from "./pages/auth/ChooseRole";
 import ClientLogin from "./pages/auth/ClientLogin";
 import ClientSignup from "./pages/auth/ClientSignup";
 import FreelancerLogin from "./pages/auth/FreelancerLogin";
 import FreelancerSignup from "./pages/auth/FreelancerSignup";
+import ForgotPassword from "./pages/auth/ForgotPassword"; 
 
 function App() {
   return (
@@ -12,10 +15,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ChooseRole />} />
+
           <Route path="/client/login" element={<ClientLogin />} />
           <Route path="/client/signup" element={<ClientSignup />} />
+
           <Route path="/freelancer/login" element={<FreelancerLogin />} />
           <Route path="/freelancer/signup" element={<FreelancerSignup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
