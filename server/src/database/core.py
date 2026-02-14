94d3d3ba0938e310af977b7d3cb9905ab8176d89
gopikapenakkuzhiyil-netizen <gopikapenakkuzhiyil@gmail.com>
2026-02-14 21:17:50 +0530

    Resolved database core merge conflict

--- server/src/database/core.py
+++ server/src/database/core.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./talentlink.db"
+DATABASE_URL = "sqlite:///./talentlink.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
