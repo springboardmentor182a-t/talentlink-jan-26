34be0b7fdd98d3d024462cde1e1987fed2f875ed
Akhila vk <akhilavk114@gmail.com>
2026-02-23 13:07:15 +0530

    Update main.py

--- server/src/main.py
+++ server/src/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.database.core import Base, engine
 from src.auth.controller import router as auth_router
 from src.proposals.controller import router as proposal_router
 
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # change to frontend URL later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth_router)
 app.include_router(proposal_router)
