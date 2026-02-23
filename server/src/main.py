0f847e625e9421fc7525a70453437c4f8f3fc062
Akhila vk <akhilavk114@gmail.com>
2026-02-23 12:50:39 +0530

    Update main.py

--- server/src/main.py
+++ server/src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from src.database.core import Base, engine
 from src.auth.controller import router as auth_router
+from src.proposals.controller import router as proposal_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(proposal_router)
