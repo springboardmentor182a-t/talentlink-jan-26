2fd9afd0a14426de219400eb70cb76e2feafd979
Akhila vk <akhilavk114@gmail.com>
2026-03-02 11:54:06 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,17 +4,23 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+// Attach JWT token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-// Submit proposal
-export const submitProposal = (data) =>
-  api.post("/proposals", data);
-
-// Get proposals (freelancer view)
-export const getMyProposals = () =>
-  api.get("/proposals/me");
-
-// Get proposals by job (client view)
-export const getProposalsByJob = (jobId) =>
-  api.get(/proposals/job/${jobId});
+// Auto-logout on 401
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
