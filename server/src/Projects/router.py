b26702b78105c1da659ad4f9d6b0d52bf96b17d8
Diya S <sdiyatvm@gmail.com>
2026-04-15 16:21:18 +0530

    fix(diya): fix file casing for router

--- server/src/Projects/router.py
+++
@@ -1,7 +0,0 @@
-@router.get("/")
-def list_projects(category: str = None, min_budget: int = 0):
-    # This filters the database based on what you select in the UI
-    query = db.query(models.Project)
-    if category:
-        query = query.filter(models.Project.category == category)
-    return query.all()
